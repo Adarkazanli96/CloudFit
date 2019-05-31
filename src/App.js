2ab138b11482bd0da0ba9decba06d6ada78dd518
Ahmed Darkazanli <adarkazanli2@gmail.com>
2019-05-30 19:49:57 -0700

    added user authentication using amazong cognito, configured with aws amplify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from "react";
 import './App.css';
-import home from './containers/home'
+import Routes from './Routes'
+import { Link, withRouter } from "react-router-dom";
+import { Nav, NavItem, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
+
 
 class App extends React.Component {
-  render(){
-    return (
-      <div className="App">
-        
-      </div>
-    );
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true
+    };
+    
+  }
+  
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  handleLogout = async event => {
+    await Auth.signOut();
+  
+    this.userHasAuthenticated(false);
+    this.props.history.push("/login");
 
   }
   
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+  
+    this.setState({ isAuthenticating: false });
+  }
+  
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+  
+    return (
+      !this.state.isAuthenticating &&
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              {this.state.isAuthenticated
+                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                : <Fragment>
+                    <LinkContainer to="/signup">
+                      <NavItem>Signup</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                      <NavItem>Login</NavItem>
+                    </LinkContainer>
+                  </Fragment>
+              }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
+      </div>
+    );
+  }  
+  
 }
 
-export default App;
+export default withRouter(App);
--- src/containers/home.js
+++ src/containers/home.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import './Home.css'
 
-class Home extends React.Component{
-    render(){
-        <div>
-
-        </div>
-    }
+let Home = () => {
+  return(
+  <div className="Home">
+  <div className="lander">
+    <h1>Scratch</h1>
+    <p>A simple note taking app</p>
+  </div>
+</div>
+);
 }
 
 export default Home
