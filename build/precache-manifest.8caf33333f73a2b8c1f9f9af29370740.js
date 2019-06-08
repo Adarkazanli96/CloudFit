4f4702885ee237a2f4bc002ab9abc3b6be5764cf
Ahmed Darkazanli <adarkazanli2@gmail.com>
2019-06-08 01:50:41 -0700

    moved build from gitignore

---
+++ build/precache-manifest.8caf33333f73a2b8c1f9f9af29370740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bffdb8f00a0309576e04f95fe8112af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af522a3c7a08b0035776",
+    "url": "/static/css/main.62c732fa.chunk.css"
+  },
+  {
+    "revision": "b24f8740950c9c48c806",
+    "url": "/static/js/2.374f978c.chunk.js"
+  },
+  {
+    "revision": "af522a3c7a08b0035776",
+    "url": "/static/js/main.1d19b2fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
